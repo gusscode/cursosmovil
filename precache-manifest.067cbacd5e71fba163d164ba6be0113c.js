924d77ab313a07b7ee06daa579f0229a26e78da1
gusscode <46688143+gusscode@users.noreply.github.com>
2020-08-05 16:05:03 -0400

    nueva interfaz

---
+++ precache-manifest.067cbacd5e71fba163d164ba6be0113c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b52c4026eac8d68655",
+    "url": "/css/about.1386fdb9.css"
+  },
+  {
+    "revision": "9ee5faf5e88daf44464d",
+    "url": "/css/app.4d08c71d.css"
+  },
+  {
+    "revision": "897bac07598d003cd107",
+    "url": "/css/chunk-vendors.be0197d9.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "eab6ac4c31d8a04d0c67f18c0548fec6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5b52c4026eac8d68655",
+    "url": "/js/about.14488107.js"
+  },
+  {
+    "revision": "9ee5faf5e88daf44464d",
+    "url": "/js/app.fd3af793.js"
+  },
+  {
+    "revision": "897bac07598d003cd107",
+    "url": "/js/chunk-vendors.5cea4037.js"
+  },
+  {
+    "revision": "beb07cc3de89e00631b3642a6d43971c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
