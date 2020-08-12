d3a9a05091bbe4d64cdd5d6e4ad5eef44add8a1c
gusscode <46688143+gusscode@users.noreply.github.com>
2020-08-11 23:36:46 -0400

    quasar

--- _nuxt/static/1597081764/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1597081764",layout:"empty",error:{statusCode:404,path:a,message:"This page could not be found"},state:{users:{user:null}},serverRendered:false,routePath:a,config:{}}}("\u002Finspire"));
